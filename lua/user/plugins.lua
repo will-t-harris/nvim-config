6bc25d6bc32c7c7141c6910642f88285514b0662
Will Harris <will.t.harris@protonmail.com>
2022-11-06 12:27:49 -0800

    update config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -59,10 +59,17 @@ return packer.startup(function(use)
   use { "lewis6991/impatient.nvim", commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6" }
   use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
   use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
+  use { "tpope/vim-surround", commit = "bf3480dc9ae7bea34c78fbba4c65b4548b5b1fea" }
+  use { "jeffkreeftmeijer/vim-numbertoggle", commit = "075b7478777e694fbac330ee34a74590dad0fee1" }
+  use { "tpope/vim-repeat", commit = "24afe922e6a05891756ecf331f39a1f6743d3d5a" }
+  use { "mg979/vim-visual-multi", commit = "724bd53adfbaf32e129b001658b45d4c5c29ca1a" }
+
 
   -- Colorschemes
   use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
   use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
+  use { "bluz71/vim-nightfly-guicolors", commit = "67192cf4a4e51f3286a9025c37e84cff128ed115" }
+  use { "maxmx03/solarized.nvim" }
 
   -- cmp plugins
   use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
