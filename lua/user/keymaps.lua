d797a045673c1f62c79dbe54b392a07dc586ac59
Will Harris <will.t.harris@protonmail.com>
2022-12-26 00:08:11 -0800

    center cursor while moving by half pages

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -21,6 +21,8 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
